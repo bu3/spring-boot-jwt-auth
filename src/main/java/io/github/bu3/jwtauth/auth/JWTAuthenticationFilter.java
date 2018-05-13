bf6c66e89de9b7a56bcd69435a9008963e73bb58
Fabio Mangione <fabio.mangione@gmail.com>
2018-05-13 16:41:30 +0100

    Removed fixed authority from authentication filter

--- src/main/java/io/github/bu3/jwtauth/auth/JWTAuthenticationFilter.java
+++ src/main/java/io/github/bu3/jwtauth/auth/JWTAuthenticationFilter.java
@@ -9,7 +9,6 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -17,7 +16,6 @@ import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Date;
 import java.util.stream.Collectors;
 
@@ -41,8 +39,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             return authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             creds.getUsername(),
-                            creds.getPassword(),
-                            Collections.singletonList(new SimpleGrantedAuthority("USER")))
+                            creds.getPassword(), null)
             );
         } catch (IOException e) {
             throw new RuntimeException(e);
